ccc3b64b924dec5c55a55393ce49c49287a61472
shazy89 <erdo.shazy123@gmail.com>
2020-10-11 08:34:42 -0400

    add booking

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -24,20 +24,20 @@ class CarsController < ApplicationController
     
     url = uploadToCloudinary(params[:car][:img])
     @car = Car.new(car_params)
+   # binding.pry
     @car.img = url
     if @car.save
-   
       render json: @car, status: :created, location: @car
     else
       render json: @car.errors, status: :unprocessable_entity
     end
   end
-  
-
-
+   
   # PATCH/PUT /cars/1
   def update
+
     if @car.update(car_params)
+
       render json: @car
     else
       render json: @car.errors, status: :unprocessable_entity
@@ -46,12 +46,15 @@ class CarsController < ApplicationController
 
   # DELETE /cars/1
   def destroy
+  
     @car.destroy
+    render json: @car
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_car
+      
       @car = Car.find(params[:id])
     end
 
@@ -63,3 +66,6 @@ class CarsController < ApplicationController
       Cloudinary::Uploader.upload(data)["url"]
     end
 end
+  
+
+
