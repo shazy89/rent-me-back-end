27134b841cf74834dc7097258fc8d277e0d6add8
shazy89 <erdo.shazy123@gmail.com>
2020-10-29 10:02:35 -0400

    test with nested routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :cars
-  resources :bookings
-  #resources :cars do 
-  # resources :bookings
-  #  end
+ # resources :cars
+ # resources :bookings
+  resources :cars do 
+   resources :bookings
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
