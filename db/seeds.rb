b0b5dbf2cac3defe366fb92bf172993c7102877a
shazy89 <erdo.shazy123@gmail.com>
2020-10-29 07:55:47 -0400

    commit for merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+@car = Car.create(make: 'Mercedes', model: '200', vehicleType: 'Suv', capacity: '4',
+    baggingCapacity: '3', rentPrice: '100', availability: true, img: 'https://res.cloudinary.com/dytheecsk/image/upload/v1602814316/qiao19heqcxnkfgilh0i.png')
